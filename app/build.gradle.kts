2234719ac501e1d67d8ac6f5f5f1be37d3497a74
Frostre_ <n9043395@gmail.com>
2026-07-29 23:09:03 +0200

    added implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
     kapt("androidx.room:room-compiler:2.6.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
