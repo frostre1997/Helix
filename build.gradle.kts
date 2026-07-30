9fadb5d56fb1782eeff134e146ebfb5684f70769
Frostre_ <n9043395@gmail.com>
2026-07-30 18:47:11 +0200

    added the Google dev tools ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "8.10.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
 }
