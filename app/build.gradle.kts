c1ff2ffd9f60fa1034c8db364c3e0ec464abcdad
Frostre_ <n9043395@gmail.com>
2026-07-30 08:00:41 +0200

    disable the universal apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
             isEnable = true
             reset()
             include("armeabi-v7a", "arm64-v8a")
-            isUniversalApk = true
+            isUniversalApk = false
         }
     }
 }
