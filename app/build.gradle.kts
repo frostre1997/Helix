29cf774c56767970003af7d0aa9ae27be7580b5a
Frostre_ <n9043395@gmail.com>
2026-07-30 17:12:50 +0200

    downgrade the GekoView to v125.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,5 +65,5 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 
-    implementation("org.mozilla.geckoview:geckoview:125.0.20240409004938")
+    implementation("org.mozilla.geckoview:geckoview:125.0")
 }
